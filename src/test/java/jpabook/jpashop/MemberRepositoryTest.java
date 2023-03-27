79834ad027acd8ca0acfd63416e1622a8fda239a
SeonHakJi <jshag90@naver.com>
2023-03-27 23:50:11 +0900

    [Feat] DB, JPA 설정 파일 작업

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+}
